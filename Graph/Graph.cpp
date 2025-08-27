1b2b57abd30bd7574b0e8713f975edb30a3ad29c
sensei38rus <dimashihow6@gmail.com>
2025-08-28 02:09:29 +0800

    graph

---
+++ Graph/Graph.cpp
@@ -0,0 +1,188 @@
+#include "Graph.h"
+#include <stdexcept>
+#include <iostream>
+#include <queue>
+#include <limits>
+#include <algorithm>
+using namespace std;
+
+void Graph::add_node(const string& id, const string& output_id, int weight) {
+	shared_ptr<Node> new_node = make_shared<Node>(id);
+	nodes[id] = new_node;
+	if (!output_id.empty()) {
+		if (nodes.count(output_id))
+			nodes[output_id]->input_nodes[id] = weight;
+		else
+			throw invalid_argument("Output node does not exists");
+	}
+}
+
+void Graph::make_connection(const string& output, const string& input, int weight) {
+	if (nodes.count(output) && nodes.count(input) && weight >= 0) {
+		nodes[output]->output_nodes[input] = weight;
+		nodes[input]->input_nodes[output] = weight;
+	}
+	else
+		throw invalid_argument("invalid arguments");
+}
+
+void Graph::erase_node(const string& id) {
+	if (nodes.count(id)) {
+		for (auto node : nodes[id]->input_nodes) {
+			nodes[node.first]->output_nodes.erase(id);
+		}
+		for (auto node : nodes[id]->output_nodes) {
+			nodes[node.first]->input_nodes.erase(id);
+		}
+		nodes.erase(id);
+	}
+	else
+		throw invalid_argument("Node does not exists");
+}
+
+void Graph::clear() {
+	nodes.clear();
+}
+
+void Graph::print_nodes() {
+	for (auto node : nodes) {
+		cout << node.first << "->";
+		for (auto output : node.second->output_nodes) {
+			cout << output.first << " & ";
+		}
+		cout << endl;
+	}
+}
+
+int Graph::distance(const string& output, const string& input) {
+	if (!nodes.count(output) || !nodes.count(input))
+		throw invalid_argument("Node or nodes do not exist");
+	return  nodes[output]->output_nodes.count(input) ? nodes[output]->output_nodes[input] : 0;
+}
+
+bool Graph::find_way_BFS(const string& A, const string& B) {
+	if (!nodes.count(A) || !nodes.count(B))
+		throw invalid_argument("Node or nodes do not exist");
+	queue<string> visits;
+	visits.push(A);
+	vector<string> visited;
+	while (!visits.empty()) {
+		if (visits.front() == B)
+			return true;
+		if (find(visited.begin(), visited.end(), visits.front()) != visited.end()) {
+			visits.pop();
+			continue;
+		}
+		for (auto e : nodes[visits.front()]->output_nodes) {
+			visits.push(e.first);
+		}
+		visited.push_back(visits.front());
+		visits.pop();
+	}
+	return false;
+
+}
+
+
+void Graph::DFS(vector<vector<string>>& paths,vector<string>& vec, const string& id, const string& goal) {
+	vector<string> curr_path = vec;
+	curr_path.push_back(id);
+	for (auto node : nodes[id]->output_nodes) {
+		if (find(curr_path.begin(), curr_path.end(), node.first) != curr_path.end())
+			continue;
+
+		if (node.first == goal) {
+			curr_path.push_back(node.first);
+			paths.push_back(curr_path);
+			return;
+		}
+		
+		DFS(paths, curr_path, node.first, goal);
+		
+	}
+}
+
+pair<vector<string>, int> Graph::find_way_DFS(const string& A, const string& B) {
+	if(!nodes.count(A) || !nodes.count(B))
+		throw invalid_argument("Node or nodes do not exist");
+	if (A == B)
+		return { {},0 };
+	vector<vector<string>> paths;
+	vector<string> path;
+	DFS(paths, path, A, B);
+	int min_distance = numeric_limits<int>::max();
+	for (auto el : paths) {
+		int sum_dist = 0;
+		for (int i = 0; i < el.size() - 1; i++) {
+		
+			sum_dist += distance(el[i], el[i + 1]);
+		}
+		
+		if (sum_dist < min_distance) {
+			min_distance = sum_dist;
+			path = el;
+		}
+	}
+	return { path, min_distance };
+	
+}
+
+void Graph::make_connections(const string& origin, const vector<pair<string, int>>& output_ids) {
+	if (!nodes.count(origin))
+		throw invalid_argument("Node does not exists");
+
+	for (auto id : output_ids) {
+		nodes[origin]->output_nodes[id.first] = id.second;
+		nodes[id.first]->input_nodes[origin] = id.second;
+	}
+}
+
+pair<vector<string>, int> Graph::dijkstra(const string& A, const string& B){
+	unordered_map<string, int> node_weight;
+	unordered_map<string, string> node_parent;
+	queue<string> check;
+	
+	for (auto node : nodes) {
+		if (node.first == A)
+			continue;
+		if (nodes[A]->output_nodes.count(node.first)) {
+			node_weight[node.first] = nodes[A]->output_nodes[node.first];
+			node_parent[node.first] = A;
+			check.push(node.first);
+		}
+		else {
+			node_weight[node.first] = numeric_limits<int>::max();
+			node_parent[node.first] = "-";
+		}
+	}
+
+	while (!check.empty()) {
+		for (auto node : nodes[check.front()]->output_nodes) {
+			if (nodes[check.front()]->output_nodes[node.first] + node_weight[check.front()] < node_weight[node.first]) {
+				node_weight[node.first] = nodes[check.front()]->output_nodes[node.first] + node_weight[check.front()];
+				node_parent[node.first] = check.front();
+				check.push(node.first);
+			}
+		}
+		check.pop();
+	}
+
+	vector<string> path{B};
+	int distance = node_weight[B];
+	auto curr_node = B;
+	while (curr_node != A) {
+		path.push_back(node_parent[curr_node]);
+		curr_node = node_parent[curr_node];
+	}
+
+	//auto last = path.end() - 1;
+	//auto first = path.begin();
+	//while (last != first) {
+	//	auto temp = *last;
+	//	*last = *first;
+	//	*first = temp;
+	//	--last;
+	//	++first;
+	//}
+	return { path, distance };
+}
---
+++ Graph/Graph.h
@@ -0,0 +1,171 @@
+#pragma once
+#include <memory>
+#include <unordered_map>
+#include <string>
+#include <vector>
+#include <iostream>
+#include <initializer_list>
+
+using namespace std;
+
+class Graph;
+class Node {
+	string id{};
+	unordered_map<string, int> output_nodes;
+	unordered_map<string, int> input_nodes;
+public:
+	Node(const string& id) : id(id){}
+	friend class Graph;
+};
+
+class Graph{
+	unordered_map<string, shared_ptr<Node>> nodes;
+	size_t size{};
+	void DFS(vector<vector<string>>& paths,vector<string>& vec, const string& id, const string& goal);
+public:
+	Graph() = default;
+	size_t get_size() { return size; }
+	void add_node(const string& id, const string& output = {}, int weight = 0);
+	void make_connection(const string& output, const string& input, int weight);
+	void make_connections(const string& id, const vector<pair<string,int>>& output_ids);
+	void erase_node(const string& id);
+	void clear();
+	void print_nodes();
+	int distance(const string& output, const string& input);
+	bool find_way_BFS(const string& A, const string& B);
+	pair<vector<string>, int> find_way_DFS(const string& A, const string& B);
+	pair<vector<string>, int> dijkstra(const string& A, const string& B);
+};
+
+
+template <typename T>
+class Binary_Tree;
+
+template <typename T>
+class Tree_Node {
+	T data{};
+	unique_ptr<Tree_Node> left{};
+	unique_ptr<Tree_Node> right{};
+public:
+	Tree_Node(const T& data) : data(data){}
+	
+	template<typename T>
+	friend class Binary_Tree;
+	
+};
+
+template <typename T>
+class Binary_Tree {
+	unique_ptr<Tree_Node<T>> root{};
+	
+	void add(unique_ptr<Tree_Node<T>>& ptr, vector<T>& vec);
+public:
+	Binary_Tree() = default;
+	Binary_Tree(const initializer_list<T>& lst);
+	void add_node(const T& value);
+	void erase_node(const T& value);
+	bool count(const T& value);
+
+	vector<T> get_nodes();
+
+};
+
+
+template <typename T>
+Binary_Tree<T>::Binary_Tree(const initializer_list<T>& lst) {
+	auto start = lst.begin();
+	while (start != lst.end()) {
+		add_node(*start);
+		++start;
+	}
+}
+
+
+
+template <typename T>
+bool Binary_Tree<T>::count(const T& value) {
+	if (!root)
+		throw invalid_argument("Empty tree");
+	auto start = root.get();
+	while (true) {
+		if (value == start->data)
+			return true;
+		if (value > start->data) {
+			if (!start->right)
+				return false;
+			start = start->right.get();
+		}
+		if (value < start->data) {
+			if (!start->left)
+				return false;
+			start = start->left.get();
+		}
+	}
+	return false;
+}
+
+template <typename T>
+void Binary_Tree<T>::add_node(const T& value) {
+	if (!root) {
+		root = make_unique<Tree_Node<T>>(value);
+		return;
+	}
+	auto start = root.get();
+	while (true) {
+		if (value == start->data)
+			return;
+		if (value > start->data) {
+			if (start->right) {
+				start = start->right.get();
+			}
+			else {
+				start->right = make_unique<Tree_Node<T>>(value);
+				
+				return;
+			}
+		}
+		if (value < start->data) {
+			if (start->left) {
+				start = start->left.get();
+			}
+			else {
+				start->left = make_unique<Tree_Node<T>>(value);
+				return;
+			}
+		}
+	}
+}
+template <typename T>
+void Binary_Tree<T>::add(unique_ptr<Tree_Node<T>>& ptr, vector<T>& vec) {
+	if (!ptr->left) {
+		vec.push_back(ptr->data);
+		if (ptr->right)
+			add(ptr->right,vec);
+		return;
+	}
+	add(ptr->left,vec);
+
+	vec.push_back(ptr->data);
+	if(ptr->right)
+		add(ptr->right,vec);
+
+}
+
+template <typename T>
+vector<T> Binary_Tree<T>::get_nodes() {
+	vector<T> vec{};
+	add(root, vec);
+	return vec;
+}
+
+template <typename T>
+void Binary_Tree<T>::erase_node(const T& value) {
+	if (!root)
+		throw invalid_argument("Node does not exists");
+	auto start = root;
+	while (start) {
+		if (value == start->data) {
+
+		}
+	}
+}
