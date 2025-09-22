859f88cac40440d9c96ee7c8301fd0db43031676
sensei38rus <dimashihow6@gmail.com>
2025-09-22 23:42:41 +0800

    test update

--- Graph/Test.cpp
+++ Graph/Test.cpp
@@ -5,7 +5,7 @@
 #include <chrono>
 #include <queue>
 #include <sstream>
-#include "C:\Users\shiho\OneDrive\Документы\headers\cpp_repos\Graph\Graph.h"
+#include "Graph.h"
 //using namespace std;
 
 int main() {
@@ -37,21 +37,26 @@ int main() {
     cout << "Execution time: " << duration.count() / 1000.0 << " milliseconds" << endl;
     cout << "Execution time: " << duration.count() / 1000000.0 << " seconds" << endl;
     */
-    auto start = chrono::high_resolution_clock::now();
+    try {
+        auto start = chrono::high_resolution_clock::now();
 
-    Graph graph("1000.txt");
-    //auto path = graph.find_way_DFS("15", "322");
-    auto path = graph.dijkstra("0", "709");
-    cout << "shortes path is:" << path.second << endl;
-    for (auto node : path.first) {
-        cout << node << " ";
+        Graph graph("1000.txt");
+        //auto path = graph.find_way_DFS("15", "322");
+        auto path = graph.dijkstra("0", "709");
+        cout << "shortes path is:" << path.second << endl;
+        for (auto node : path.first) {
+            cout << node << " ";
+        }
+        cout << endl;
+        auto end = chrono::high_resolution_clock::now();
+        auto duration = duration_cast<chrono::microseconds>(end - start);
+
+        cout << "Execution time: " << duration.count() << " mircoseconds" << endl;
+        cout << "Execution time: " << duration.count() / 1000.0 << " milliseconds" << endl;
+        cout << "Execution time: " << duration.count() / 1000000.0 << " seconds" << endl;
     }
-    cout << endl;
-    auto end = chrono::high_resolution_clock::now();
-    auto duration = duration_cast<chrono::microseconds>(end - start);
-
-    cout << "Execution time: " << duration.count() << " mircoseconds" << endl;
-    cout << "Execution time: " << duration.count() / 1000.0 << " milliseconds" << endl;
-    cout << "Execution time: " << duration.count() / 1000000.0 << " seconds" << endl;
+    catch (const exception& e) {
+        cout << e.what() << endl;
+    }
     return 0;
 }
